d4fb91be42129126c44f95c69524f50fe45bc346
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2017-05-04 16:22:47 -0300

    Create step runner; v0.0.1

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ERR_MSG=()
+if [ ! -n "$WERCKER_SENTRY_RELEASE_AUTH_TOKEN" ]; then
+  ERR_MSG+=("Please specify auth_token property")
+fi
+
+if [ ! -n "$WERCKER_SENTRY_RELEASE_ORGANIZATION" ]; then
+  ERR_MSG+=("Please specify organization property")
+fi
+
+if [ -n "$ERR_MSG" ]; then
+  printf '\033[0;91m%s\n\033[0m' "${ERR_MSG[@]}"
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_SENTRY_RELEASE_VERSION" ]; then
+  export WERCKER_SENTRY_RELEASE_VERSION=$WERCKER_GIT_COMMIT
+fi
+
+SENTRY_ENTRYPOINT="https://sentry.io/api/0/organizations/$WERCKER_SENTRY_RELEASE_ORGANIZATION/releases/"
+
+curl -v $SENTRY_ENTRYPOINT \
+  -X POST \
+  -H "Authorization: Bearer $WERCKER_SENTRY_RELEASE_AUTH_TOKEN" \
+  -H 'Content-Type: application/json' \
+  -d "
+  {
+    \"version\": \"$WERCKER_SENTRY_RELEASE_VERSION\",
+    \"refs\": [{
+        \"repository\":\"$WERCKER_GIT_REPOSITORY\",
+        \"commit\":\"$WERCKER_GIT_COMMIT\",
+    }]
+  }
+"
